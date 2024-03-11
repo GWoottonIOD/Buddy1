0edc0e831d8daec98412e76b71f22e8132168d95
Gareth Wootton <gareth.wootton@institutedata.com>
2024-03-11 18:35:47 +1300

    PaidDelete now updates user balance when debt is paid.

--- src/components/transactions/PaidDeleteComponent.jsx
+++ src/components/transactions/PaidDeleteComponent.jsx
@@ -6,7 +6,7 @@ import DoneIcon from '@mui/icons-material/Done';
 import EditIcon from '@mui/icons-material/Edit';
 import { DebtContext } from '../../context/DebtContext';
 import DraggableDialog from '../editTransaction/DraggableDialog';
-import { updateQuery } from '../../axios/AxiosFunctions';
+import { updateQuery, readQuery } from '../../axios/AxiosFunctions';
 
 export default function PaidDeleteComponent({ debt }) {
     const { setDebts, debts } = useContext(DebtContext);
@@ -62,7 +62,12 @@ export default function PaidDeleteComponent({ debt }) {
     return (
         <>
             {debt.paid?null:<Button size="small" 
-                onClick={() => updateQuery('debts', { 'id': debt.id, 'paid': true })}
+                onClick={() => 
+                    readQuery('users', debt.userID )
+                    .then((response) => updateQuery('users', { 'id': debt.userID, 'total': parseInt(response[0].total - debt.amount) }))
+                    .then(() => updateQuery('debts', { 'id': debt.id, 'paid': true }))
+                    .then(() => setDebts(debts.filter((transaction) => transaction.id !== debt.id)))
+                }
                 sx={{'&&:focus': {outline: 'none'}}}><DoneIcon/>
             </Button>}
             <Button size="small" 
