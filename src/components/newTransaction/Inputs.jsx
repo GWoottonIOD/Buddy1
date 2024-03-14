fd0fa63d5abeba1331d268c00ae33850eeaeba50
Gareth Wootton <gareth.wootton@institutedata.com>
2024-03-15 09:28:05 +1300

    Fixed double post to BE of debts by getting rid of publishDebt and trimmed inputs to use promises.

--- src/components/newTransaction/Inputs.jsx
+++ src/components/newTransaction/Inputs.jsx
@@ -1,28 +1,31 @@
 import React, {useState} from 'react'
 import {Button} from '@mui/material'
 import DateChange from './DateChange';
-import PublishDebt from './PublishDebt';
+import { updateQuery, createQuery } from '../../axios/AxiosFunctions';
 import ChangeAmount from './ChangeAmount';
+import { useNavigate } from 'react-router-dom';
 
 export default function Inputs(props) {
     const [dueDate, setDueDate] = useState(null);
     const [amount, setAmount] = useState(null)
-    const [count, setCount] = useState(0)
+    const navigate = useNavigate();
 
-    const inputCheck = () => {
-        if (amount == null || dueDate == null || props.user == null) {
-            alert('Please fill in all fields')
-        } else {setCount(count+1)}
-    }
+    const sum = parseInt(props.user.total + amount)
+    const debt = { userID: props.userId, amount: amount, duedate: dueDate, 
+        total: sum, paid: false }
     
     return (
         <div>
             <ChangeAmount setAmount={setAmount}/>
             <DateChange setDueDate={setDueDate}/><br/>
-            <Button onClick={inputCheck}>Add</Button>
-            {count===1 
-                ? <PublishDebt user={props.user} userId={props.userId} amount={amount} dueDate={dueDate}/> 
-                : null}
+            <Button onClick={amount == null || dueDate == null || props.user == null
+            ? null
+            : () => createQuery('debts', debt)
+            .then(()=> updateQuery('users', { id: props.userId , total: sum }))
+            .then(()=> navigate('/'))
+            }>
+                Add
+            </Button>
         </div>
     )
 }
